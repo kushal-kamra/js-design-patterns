6c51892f9484fb6bafab11128ea7e3cc3c5a20b2
kushal-kamra <kushalkamra@gmail.com>
2020-05-11 12:19:18 +0530

    Abstract Factory Pattern

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Prototyping Class Pattern
+// Prototyping Class Pattern
 
 class Car {
     constructor(doors, engine, color) {
@@ -12,7 +12,7 @@ const civic = new Car(4, 'V6', 'Red');
 
 console.log(civic);
 
-//Constructor Pattern
+// Constructor Pattern
 
 class SUV extends Car {
     constructor(doors, engine, color, wheels) {
@@ -25,7 +25,7 @@ const cx5 = new SUV(5, 'V8', 'Blue', 4);
 
 console.log(cx5);
 
-//Singleton Pattern
+// Singleton Pattern
 
 let instance = null;
 
@@ -69,10 +69,44 @@ class CycleFactory {
     }
 }
 
-const factory = new CycleFactory();
+const cycleFactory = new CycleFactory();
 
-const myOkCycle = factory.createCycle('ok');
+const myOkCycle = cycleFactory.createCycle('ok');
 console.log(myOkCycle);
 
-const myGoodCycle = factory.createCycle('good');
+const myGoodCycle = cycleFactory.createCycle('good');
 console.log(myGoodCycle);
+
+// Abstract Factory Pattern
+
+class CarFactory {
+    createCar(model) {
+        switch(model) {
+            case 'civic':
+                return new Car(4, 'V6', 'Red');
+            case 'esteem':
+                return new Car(4, 'V4', 'Yellow');
+        }
+    }
+}
+
+const carFactory = new CarFactory();
+
+class VehicleFactory {
+    createVehicle(type, model) {
+        switch(type) {
+            case 'cycle':
+                return cycleFactory.createCycle(model);
+            case 'car':
+                return carFactory.createCar(model);
+        }
+    }
+}
+
+const vehicleFactory = new VehicleFactory();
+
+const vehicleGoodCycle = vehicleFactory.createVehicle('cycle', 'good');
+console.log(vehicleGoodCycle);
+
+const vehicleCivicCar = vehicleFactory.createVehicle('car', 'civic');
+console.log(vehicleCivicCar);
