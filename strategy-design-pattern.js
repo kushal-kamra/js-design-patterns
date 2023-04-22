1bcb9570ee457cdeb16db5026a25e88e2a966bde
kushal-kamra <kushalkamradev@gmail.com>
2023-04-22 14:24:38 +0530

    Added example for strategy pattern and constructor injection

---
+++ strategy-design-pattern.js
@@ -0,0 +1,51 @@
+class DriveStrategy {
+    drive() {
+        throw new error ("Implement drive!");
+    }
+};
+
+class NormalDriveStrategy extends DriveStrategy {
+    drive() {
+        console.log("Normal drive capability");
+    }
+};
+
+class SportsDriveStrategy extends DriveStrategy {
+    drive() {
+        console.log("Sports drive strategy");
+    }
+};
+
+class Vehicle {
+    constructor(driveObj) {
+        this.driveObject = driveObj;
+    }
+
+    drive() {
+        this.driveObject.drive();
+    }
+}
+
+class OffroadVehicle extends Vehicle {
+    constructor() {
+        super(new SportsDriveStrategy());
+    }
+}
+
+class SportsVehicle extends Vehicle {
+    constructor() {
+        super(new SportsDriveStrategy());
+    }
+}
+
+class GoodsVehicle extends Vehicle {
+    constructor() {
+        super(new NormalDriveStrategy());
+    }
+}
+
+let vehicle_1 = new SportsVehicle();
+vehicle_1.drive();
+
+let vehicle_2 = new GoodsVehicle();
+vehicle_2.drive();
